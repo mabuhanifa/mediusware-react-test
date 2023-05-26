0db91e3898a88fa0b52925ba3302c8c127f11801
shourovahmd <moh.abuhanifa@gmail.com>
2023-05-26 15:19:06 +0600

    conditional url

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,6 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-export default function Modal({modal}) {
+export default function Modal({ modal, setModal, url, setUrl }) {
+  const [modalData, setData] = useState([]);
+  const setALL = (link) => {
+    setModal(link);
+    if (link === "Modal-A") {
+      setUrl(
+        "https://contact.mediusware.com/api/contacts/?page=1&page_size=1000"
+      );
+    } else if (link === "Modal-B") {
+      setUrl(
+        "https://contact.mediusware.com/api/country-contacts/United%20States/"
+      );
+    }
+  };
+ 
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(url);
+      const data = await response.json();
+      setData(data.results);
+    };
+    fetchData();
+  }, [url]);
+  console.log(modalData);
   return (
     <div
       className="modal fade"
@@ -13,8 +36,9 @@ export default function Modal({modal}) {
         <div className="modal-content">
           <div className="modal-header">
             <h5 className="modal-title" id={`${modal}Label`}>
-              Modal title
+              {modal}
             </h5>
+
             <button
               type="button"
               className="btn-close"
@@ -25,14 +49,23 @@ export default function Modal({modal}) {
           <div className="modal-body">...</div>
           <div className="modal-footer">
             <button
+              className="btn btn-primary"
+              onClick={() => setALL("Modal-A")}
+            >
+              All Contacts
+            </button>
+            <button
+              className="btn btn-warning"
+              onClick={() => setALL("Modal-B")}
+            >
+              US Contacts
+            </button>
+            <button
               type="button"
               className="btn btn-secondary"
               data-bs-dismiss="modal"
             >
               Close
-            </button>
-            <button type="button" className="btn btn-primary">
-              Save changes
             </button>
           </div>
         </div>
--- src/components/Problem-2.jsx
+++ src/components/Problem-2.jsx
@@ -3,20 +3,34 @@ import Modal from "./Modal";
 
 const Problem2 = () => {
   const [modal, setModal] = useState("Modal");
+  const [url, setUrl] = useState("");
 
+  const setALL = (link) => {
+    setModal(link);
+    if (link === "Modal-A") {
+      setUrl(
+        "https://contact.mediusware.com/api/contacts/?page=1&page_size=1000"
+      );
+    } else if (link === "Modal-B") {
+      setUrl(
+        "https://contact.mediusware.com/api/country-contacts/United%20States/"
+      );
+    }
+  };
+  
   return (
     <div className="container">
       <div className="row justify-content-center mt-5">
         <h4 className="text-center text-uppercase mb-5">Problem-2</h4>
 
         <div className="d-flex justify-content-center gap-3">
-          <Modal modal={modal} />
+          <Modal modal={modal} setModal={setModal} url={url} setUrl={setUrl}/>
           <button
             className="btn btn-lg btn-outline-primary"
             type="button"
             data-bs-toggle="modal"
             data-bs-target={`#${modal}`}
-            onClick={() => setModal("ModalA")}
+            onClick={() => setALL("Modal-A")}
           >
             All Contacts
           </button>
@@ -25,7 +39,7 @@ const Problem2 = () => {
             type="button"
             data-bs-toggle="modal"
             data-bs-target={`#${modal}`}
-            onClick={() => setModal("ModalB")}
+            onClick={() => setALL("Modal-B")}
           >
             US Contacts
           </button>
