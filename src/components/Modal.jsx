53922827f8a39612f4125607b97219a9621b85fa
shourovahmd <moh.abuhanifa@gmail.com>
2023-05-26 14:26:08 +0600

    Modal

---
+++ src/components/Modal.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+export default function Modal({modal}) {
+  return (
+    <div
+      className="modal fade"
+      id={modal}
+      tabIndex="-1"
+      aria-labelledby={`${modal}Label`}
+      aria-hidden="true"
+    >
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title" id={`${modal}Label`}>
+              Modal title
+            </h5>
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div className="modal-body">...</div>
+          <div className="modal-footer">
+            <button
+              type="button"
+              className="btn btn-secondary"
+              data-bs-dismiss="modal"
+            >
+              Close
+            </button>
+            <button type="button" className="btn btn-primary">
+              Save changes
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/Problem-2.jsx
+++ src/components/Problem-2.jsx
@@ -1,21 +1,38 @@
-import React from 'react';
+import React, { useState } from "react";
+import Modal from "./Modal";
 
 const Problem2 = () => {
+  const [modal, setModal] = useState("Modal");
 
-    return (
+  return (
+    <div className="container">
+      <div className="row justify-content-center mt-5">
+        <h4 className="text-center text-uppercase mb-5">Problem-2</h4>
 
-        <div className="container">
-            <div className="row justify-content-center mt-5">
-                <h4 className='text-center text-uppercase mb-5'>Problem-2</h4>
-                
-                <div className="d-flex justify-content-center gap-3">
-                <button className="btn btn-lg btn-outline-primary" type="button" >All Contacts</button>
-                <button className="btn btn-lg btn-outline-warning" type="button" >US Contacts</button>
-                </div>
-                
-            </div>
+        <div className="d-flex justify-content-center gap-3">
+          <Modal modal={modal} />
+          <button
+            className="btn btn-lg btn-outline-primary"
+            type="button"
+            data-bs-toggle="modal"
+            data-bs-target={`#${modal}`}
+            onClick={() => setModal("ModalA")}
+          >
+            All Contacts
+          </button>
+          <button
+            className="btn btn-lg btn-outline-warning"
+            type="button"
+            data-bs-toggle="modal"
+            data-bs-target={`#${modal}`}
+            onClick={() => setModal("ModalB")}
+          >
+            US Contacts
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Problem2;
